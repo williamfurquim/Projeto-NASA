35a7f4ae8c6202c4b557b50f501a79f0db8d2d27
williamfurquim <w.furquim@aluno.senai.br>
2025-10-05 23:12:08 -0300

    Update compartilhamento.js

--- JS/compartilhamento.js
+++ JS/compartilhamento.js
@@ -48,7 +48,7 @@ onAuthStateChanged(auth, (firebaseUser) => {
     user.name = firebaseUser.email || "Anonymous";
     user.color = getRandomColor();
 
-    websocket = new WebSocket("wss://api-de-conversa-da-nasa.onrender.com");
+    websocket = new WebSocket("wss://chat-backend-cyeu.onrender.com");
     websocket.onmessage = processMessage;
   }
 });
@@ -137,3 +137,4 @@ const sendMessage = (event) => {
 
 
 chatForm.addEventListener("submit", sendMessage)
+
